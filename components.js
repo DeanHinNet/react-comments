0fea18ecec02dd0db9a6c2892408204811e921e0
dean <hindrough@gmail.com>
2017-08-20 23:01:57 -0700

    added state

--- components.js
+++ components.js
@@ -18,23 +18,44 @@ class Comment extends React.Component {
 }
 
 class CommentBox extends React.Component {
+	constructor(){
+		//initializing state of 'showComments'
+		super();
 
+		this.state = {
+			showComments: false
+		};
+	}
+	
 	render(){
 		const comments = this._getComments();
+		let buttonText = 'Show Comments';
+		let commentNodes;
+
+		if(this.state.showComments){
+			buttonText = 'Hide Comments';
+			commentNodes = <div className='comment-list'>{comments}</div>;
+		}
+
 		return(
+			
 			<div className='comment-box'>
+				<button onClick={this._handleClick.bind(this)}>{buttonText}</button>
 				<h3>Comments</h3>
 				<h4 className='comment-count'>{this._getCommentsTitle(comments.length)}</h4>
-				<div className='comment-list'>
-					{comments}
-				</div>
+					{commentNodes}
 			</div>
 		)
+	}
+	_handleClick(){
+		this.setState({
+			showComments: !this.state.showComments
+		});
 	}
 
 	_getComments(){
 		const commentList = [
-			{id: 1, author: 'Morgan McCircuit', body: 'Great Picture'},
+			{id: 1, author: 'Drogon McCircuit', body: 'Great Picture'},
 			{id: 2, author: 'Bending Bender', body: 'Excellent Stuff'}
 		];
 
